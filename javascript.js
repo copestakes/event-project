30a623792f3013a6fbcfab2b20b6cad38c35a74e
Copestakes <davecopestakes@gmail.com>
2022-12-01 09:50:09 -0500

    updated files

---
+++ javascript.js
@@ -0,0 +1,31 @@
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true
+});
+
+// element argument can be a selector string
+//   for an individual element
+var flkty = new Flickity( '.main-carousel', {
+  // options
+});
+
+
+
+{/* <script src="/path/to/flickity.pkgd.min.js"></script>
+
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'center',
+  contain: true,
+  autoPlay: true,
+  wrapAround: true,
+});
+
+// element argument can be a selector string
+//   for an individual element
+var flkty = new Flickity( '.main-carousel', {
+  // options
+}); */}
